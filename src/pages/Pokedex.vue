03c84105363b70f5bf3a99e51c5afd95e2954631
Javier Pomachagua <javierpp0303@gmail.com>
2020-10-23 20:02:49 -0500

    PokeApi

---
+++ src/pages/Pokedex.vue
@@ -0,0 +1,43 @@
+<template>
+  <Layout>
+    <div
+      class="bg-blue-500 rounded-lg flex flex-col items-center justify-center p-5 space-y-5"
+    >
+      <h1 class="text-lg font-semibold text-white uppercase">
+        ¡Elige tu primer Pokemón!
+      </h1>
+      <div class="flex flex-wrap">
+        <g-link
+          :to="`/pokemon/${edge.node.id}`"
+          v-for="edge in $page.pokemons.edges"
+          :key="edge.node.id"
+          class="m-2 flex flex-col items-center"
+        >
+          <div class="w-20 h-20 p-2 border-purple-600 border-2 border-solid rounded-full bg-white">
+            <img :src="edge.node.image" alt="" class="object-cover" />
+          </div>
+
+          <p class="font-semibold capitalize">{{ edge.node.name }}</p>
+        </g-link>
+      </div>
+    </div>
+  </Layout>
+</template>
+
+<page-query>
+query {
+  pokemons: allPokemon {
+    edges {
+      node {
+        id
+        name,
+        image
+      }
+    }
+  }
+}
+</page-query>
+
+<style>
+
+</style>
---
+++ src/templates/Pokemon.vue
@@ -0,0 +1,47 @@
+<template>
+  <Layout>
+    <div
+      class="bg-blue-500 rounded-lg flex flex-col items-center justify-center p-8"
+    >
+      <h1 class="text-lg font-semibold mb-4">
+        Escogiste a
+        <span class="text-xl text-green-300 uppercase font-bold">{{
+          $page.pokemon.name
+        }}</span>
+      </h1>
+      <div
+        class="w-32 h-32 rounded-full bg-white border-purple-600 border-2 border-solid p-2 mb-4"
+      >
+        <img :src="$page.pokemon.image" alt="" />
+      </div>
+      <g-link
+        to="/pokedex"
+        class="bg-yellow-500 rounded-lg font-bold text-md px-4 py-2"
+      >Regresar</g-link>
+    </div>
+  </Layout>
+</template>
+
+<page-query>
+query ($id: ID!) {
+  pokemon(id: $id) {
+    id
+    name
+    image
+  }
+}
+</page-query>
+
+<script>
+export default {
+  metaInfo() {
+    return {
+      name: this.$page.pokemon.name
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
